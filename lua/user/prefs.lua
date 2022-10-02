a1c25ad6a8256c82820533ce7ea57e72f207337e
TeaSpillStudios <teaspillstudios@proton.me>
2022-10-02 03:35:36 +0100

    Add projects keybidning (`<leader>fp`)

--- lua/user/prefs.lua
+++ lua/user/prefs.lua
@@ -23,6 +23,7 @@ vim.cmd('nnoremap <leader>ff <cmd>Telescope find_files<cr>')
 vim.cmd('nnoremap <leader>fg <cmd>Telescope live_grep<cr>')
 vim.cmd('nnoremap <leader>fb <cmd>Telescope buffers<cr>')
 vim.cmd('nnoremap <leader>fh <cmd>Telescope help_tags<cr>')
+vim.cmd('nnoremap <leader>fp <cmd>Telescope projects<cr>')
 
 vim.cmd('nnoremap <leader>t <cmd>TroubleToggle<cr>')
 
