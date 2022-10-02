8c2fa9d140035c29f891e23f0bcdc5cf57d4f8e9
TeaSpillStudios <teaspillstudios@proton.me>
2022-10-02 02:02:25 +0100

    Make escape cancel the recent most recent search

--- lua/user/prefs.lua
+++ lua/user/prefs.lua
@@ -12,3 +12,6 @@ vim.cmd('nnoremap <leader>ff <cmd>Telescope find_files<cr>')
 vim.cmd('nnoremap <leader>fg <cmd>Telescope live_grep<cr>')
 vim.cmd('nnoremap <leader>fb <cmd>Telescope buffers<cr>')
 vim.cmd('nnoremap <leader>fh <cmd>Telescope help_tags<cr>')
+
+-- Make escape cancel the recent most recent search
+vim.cmd('nnoremap <esc> :noh<return><esc>')
