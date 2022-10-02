c7d1c7737544d745f3a35ba770a0314c6e723ff5
TeaSpillStudios <teaspillstudios@proton.me>
2022-10-02 02:00:40 +0100

    Setup gitsigns, add telescope keybinds and change listchars tab

--- lua/user/prefs.lua
+++ lua/user/prefs.lua
@@ -1,7 +1,14 @@
 vim.cmd('set number relativenumber')
 vim.cmd('set clipboard=unnamedplus')
 vim.cmd('set expandtab shiftwidth=2')
-vim.cmd('set list listchars=tab:▸▸,trail:·')
+--vim.cmd('set list listchars=tab:▸▸,trail:·')
+vim.cmd('set list listchars=tab:··,trail:·')
 vim.cmd('set mouse=a')
 vim.cmd('let mapleader=","')
 vim.cmd('nnoremap <leader><leader> :b#<CR>')
+
+-- Find files using Telescope command-line sugar.
+vim.cmd('nnoremap <leader>ff <cmd>Telescope find_files<cr>')
+vim.cmd('nnoremap <leader>fg <cmd>Telescope live_grep<cr>')
+vim.cmd('nnoremap <leader>fb <cmd>Telescope buffers<cr>')
+vim.cmd('nnoremap <leader>fh <cmd>Telescope help_tags<cr>')
